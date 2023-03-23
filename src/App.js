d21cf34b2aeadec3478df58b57de5ff537375d78
DaniloCTM <42286412+DaniloCTM@users.noreply.github.com>
2023-03-23 10:46:19 -0300

    API funcionando

--- src/App.js
+++ src/App.js
@@ -1,23 +1,31 @@
-import logo from './logo.svg';
 import './App.css';
+import Tmdb from './Tmdb';
+import React, {useEffect, useState} from 'react';
+import MovieRow from './Components/movieRow';
 
 function App() {
+  
+  const [movieList, setMovieList] = useState([]);
+
+  // Quando a pagina é carregada o codigo dentro do useEffect é executado
+  useEffect( () => {
+    const loadAll = async () => {
+      //pegando a lista de filmes
+      let list = await Tmdb.getHomeList();
+      setMovieList(list);
+      console.log(list)
+    }
+
+    loadAll();
+  }, []);
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='home-page'>
+      <section className="lists">
+        {movieList.map((item, key) => (
+          <MovieRow key={key} title={item.title} items={item.items}/>
+        ))}
+      </section>
     </div>
   );
 }
---
+++ src/Tmdb.js
@@ -0,0 +1,36 @@
+//Todas as requisições serão feitas por meio desse arquivo
+
+const API_KEY = '84c2e94e58561d2845fbf2de10a1a1a5';
+const API_BASE =  'https://api.themoviedb.org/3';
+
+/*
+- Mais assistidos
+- Proximos lançamentos
+- Ação
+- Comédia
+- Terror
+*/
+
+//O await faz a função esperar a resposta do request para poder continuar executando o codigo
+const basicFetch = async (endpoint) => {
+    const req = await fetch(`${API_BASE}${endpoint}`);
+    const json = await req.json();
+    return json;
+}
+
+export default {
+    getHomeList: async () => {
+        return [
+            {
+                slug:'originals',
+                title: 'Originais do Netflix',
+                items: await basicFetch(`/discover/tv?with_network=213&language=pt-BR&api_key=${API_KEY}`)
+            },
+            {
+                slug:'action', 
+                title: 'Ação',
+                items: await basicFetch(`/discover/movie?with_genres=28&language=pt-BR&api_key=${API_KEY}`)
+            },
+        ];
+    }
+}
