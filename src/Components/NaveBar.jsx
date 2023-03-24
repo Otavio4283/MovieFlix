c993371d0109b0ab4b60d1d710a210eae7b97f6d
Amanda De Moura Da Silva <110147898+AmandaMourx@users.noreply.github.com>
2023-03-24 12:49:20 -0300

    adicionando o icone de pesquisa e o butao de login

--- src/Components/NaveBar.jsx
+++ src/Components/NaveBar.jsx
@@ -1,11 +1,13 @@
 import {React} from 'react';
 import './NaveBar.css'
 import SearchIcon from '@mui/icons-material/Search';
+import Button from '@mui/material/Button';
 
 export default function NaveBar () {
   return (
     <nav>
       <div className='main_name'>MovieFlix</div>
+      <Button className='login_button'  style={{ borderWidth: '3px', borderColor: 'white', color: 'white', fontWeight:'bold', left: '78%', fontSize:'17px', justifyContent:'center', alignItems:'center' }} variant="outlined">LOGIN</Button>
       <div class="links">
         <ul class='categories'>
           <il><a href='#acao'></a>Ação</il>
@@ -13,9 +15,7 @@ export default function NaveBar () {
           <il><a href='#comedia'></a>Comédia</il>
         </ul>
         <div className='search'>
-          <div className='search_icon'>
-            
-          </div>
+          <SearchIcon className='search_icon'/>
         </div>
       </div>
     </nav>
